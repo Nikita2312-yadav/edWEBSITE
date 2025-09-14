a9fd134785cebcd8181020fb3884fd74d1ca9b26
Nikki0723-s <nikkiyadavv627@gmail.com>
2025-09-14 17:20:34 +0530

    check

--- Server/server.js
+++ Server/server.js
@@ -30,7 +30,7 @@ app.post('/stripe', express.raw({ type: 'application/json'}), stripeWebhooks)
           
 //Port               
 const PORT = process.env.PORT || 5000  
-        
+          
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 }) 
